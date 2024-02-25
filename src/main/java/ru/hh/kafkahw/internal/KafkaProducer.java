b7d397ce5c4bc9fce1e0949ab83078cee3be6dd1
Maxim Orlov <orl2011.2011@gmail.com>
2024-02-25 14:08:23 +0300

    Returned KafkaProducer to initial state

--- src/main/java/ru/hh/kafkahw/internal/KafkaProducer.java
+++ src/main/java/ru/hh/kafkahw/internal/KafkaProducer.java
@@ -22,12 +22,7 @@ public class KafkaProducer {
       throw new RuntimeException();
     }
     LOGGER.info("send to kafka, topic {}, payload {}", topic, payload);
-    if ("topic3".equals(topic)) {
-      kafkaTemplate.send(topic, 1, payload, payload);
-    }
-    else {
-      kafkaTemplate.send(topic, payload);
-    }
+    kafkaTemplate.send(topic, payload);
     if (random.nextInt(100) < 2) {
       LOGGER.warn("silent delivery, topic {}, payload {}", topic, payload);
       throw new RuntimeException();
